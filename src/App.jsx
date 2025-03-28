866e7baa177a27ffcacd865bfe67aae9b69a6938
alexciccarese <alex.ciccarese@gmail.com>
2025-03-28 16:19:50 +0100

    add Bootstrap 5.3.3 and implement basic data fetching in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from 'react'
+import { useEffect } from "react"
+const api_url = 'http://localhost:3003/posts' 
 
+export default function App() {
 
-function App() {
+  useEffect(() => {
 
-  return (
-    <>
+    fetchData(api_url)
+  }, [])
 
-    </>
+  function fetchData(url) {
+    fetch(url)
+    .then(res => res.json)
+    .then(data => {
+      console.log(data);
+      
+    })
+  }
+
+  return(
+
+    <h1>Hello!</h1>
   )
 }
-
-export default App
